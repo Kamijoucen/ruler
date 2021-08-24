abbffa714e1af9c9a9e57ac9a6e2951676ed10c7
kamijoucen <kamijoucen@foxmail.com>
2021-08-24 23:48:13 +0800

    ..

--- src/main/java/com/kamijoucen/ruler/compiler/RulerCompiler.java
+++ src/main/java/com/kamijoucen/ruler/compiler/RulerCompiler.java
@@ -2,9 +2,14 @@ package com.kamijoucen.ruler.compiler;
 
 public class RulerCompiler {
 
+    private String basePath;
 
     public void compile() {
+
     }
+
+
+
 
 
 }
---
+++ src/main/java/com/kamijoucen/ruler/module/Script.java
@@ -0,0 +1,15 @@
+package com.kamijoucen.ruler.module;
+
+public class Script {
+
+    private String fileName;
+
+    private String baseName;
+
+    private String moduleName;
+
+    private String content;
+
+
+
+}
