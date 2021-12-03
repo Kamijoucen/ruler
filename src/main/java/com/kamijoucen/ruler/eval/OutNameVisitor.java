7320eba28f5f235dd968ae824e6d5e3e485be5b9
kamijoucen <kamijoucen@foxmail.com>
2021-12-03 11:04:34 +0800

    outname

---
+++ src/main/java/com/kamijoucen/ruler/eval/OutNameVisitor.java
@@ -0,0 +1,14 @@
+package com.kamijoucen.ruler.eval;
+
+import com.kamijoucen.ruler.ast.facotr.OutNameNode;
+import com.kamijoucen.ruler.runtime.RuntimeContext;
+import com.kamijoucen.ruler.runtime.Scope;
+import com.kamijoucen.ruler.value.BaseValue;
+
+public class OutNameVisitor extends AbstractVisitor {
+    @Override
+    public BaseValue eval(OutNameNode node, Scope scope, RuntimeContext context) {
+        context.getOutNameToken().add(node.name);
+        return super.eval(node, scope, context);
+    }
+}
--- src/main/java/com/kamijoucen/ruler/runtime/RuntimeContext.java
+++ src/main/java/com/kamijoucen/ruler/runtime/RuntimeContext.java
@@ -1,16 +1,18 @@
 package com.kamijoucen.ruler.runtime;
 
 import com.kamijoucen.ruler.eval.NodeVisitor;
+import com.kamijoucen.ruler.token.Token;
 import com.kamijoucen.ruler.value.BaseValue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class RuntimeContext {
 
     private Boolean isCallLinkAssign = null;
-
+    private List<Token> outNameToken = new ArrayList<Token>();
     private Map<String, BaseValue> outSpace;
-
     private final NodeVisitor nodeVisitor;
 
     public RuntimeContext(Map<String, BaseValue> outSpace, NodeVisitor nodeVisitor) {
@@ -32,5 +34,13 @@ public class RuntimeContext {
 
     public NodeVisitor getNodeVisitor() {
         return nodeVisitor;
+    }
+
+    public List<Token> getOutNameToken() {
+        return outNameToken;
+    }
+
+    public void setOutNameToken(List<Token> outNameToken) {
+        this.outNameToken = outNameToken;
     }
 }
