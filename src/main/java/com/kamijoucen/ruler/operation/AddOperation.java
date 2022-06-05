b8863bfd74c652eb414e8c0b25b7890f777ab0c6
kamijoucen <kamijoucen@foxmail.com>
2022-06-05 20:34:00 +0800

    字符串相加

--- src/main/java/com/kamijoucen/ruler/operation/AddOperation.java
+++ src/main/java/com/kamijoucen/ruler/operation/AddOperation.java
@@ -2,10 +2,7 @@ package com.kamijoucen.ruler.operation;
 
 import com.kamijoucen.ruler.exception.SyntaxException;
 import com.kamijoucen.ruler.runtime.RuntimeContext;
-import com.kamijoucen.ruler.value.BaseValue;
-import com.kamijoucen.ruler.value.DoubleValue;
-import com.kamijoucen.ruler.value.IntegerValue;
-import com.kamijoucen.ruler.value.ValueType;
+import com.kamijoucen.ruler.value.*;
 
 import java.util.Arrays;
 
@@ -30,6 +27,8 @@ public class AddOperation implements Operation {
             DoubleValue val1 = (DoubleValue) tempVal1;
             DoubleValue val2 = (DoubleValue) tempVal2;
             return new DoubleValue(val1.getValue() + val2.getValue());
+        } else if (tempVal1.getType() == ValueType.STRING || tempVal2.getType() == ValueType.STRING) {
+            return new StringValue(tempVal1.toString() + tempVal2.toString());
         }
         throw SyntaxException.withSyntax("该值不支持做加法:" + Arrays.toString(param));
     }
