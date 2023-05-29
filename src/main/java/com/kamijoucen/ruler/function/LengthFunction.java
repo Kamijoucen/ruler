87c6f03f0ff40541a74e0a99e31d5e2f82c8d425
kamijoucen <kamijoucen@foxmail.com>
2023-05-29 16:40:35 +0800

    class fix

--- src/main/java/com/kamijoucen/ruler/function/LengthFunction.java
+++ src/main/java/com/kamijoucen/ruler/function/LengthFunction.java
@@ -9,7 +9,7 @@ public class LengthFunction implements RulerFunction {
 
     @Override
     public String getName() {
-        return "length";
+        return "Length";
     }
 
     @Override
--- src/main/java/com/kamijoucen/ruler/value/RClass.java
+++ src/main/java/com/kamijoucen/ruler/value/RClass.java
@@ -2,10 +2,6 @@ package com.kamijoucen.ruler.value;
 
 import java.util.Map;
 
-// add是没有，需要添加一个
-// set是有key，但是没有value
-// put是没有key
-
 public interface RClass extends BaseValue {
 
     Map<String, BaseValue> getProperties();
