a3b9a34b6af388a8d3e42e9b7c66e5fe41a4e267
kamijoucen <kamijoucen@foxmail.com>
2024-04-19 17:10:16 +0800

    fix

--- src/main/java/com/kamijoucen/ruler/config/impl/SpiLoaderManagerImpl.java
+++ src/main/java/com/kamijoucen/ruler/config/impl/SpiLoaderManagerImpl.java
@@ -1,17 +1,26 @@
 package com.kamijoucen.ruler.config.impl;
 
 import java.util.ServiceLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.kamijoucen.ruler.config.RulerConfiguration;
 import com.kamijoucen.ruler.config.SpiLoaderManager;
 import com.kamijoucen.ruler.plugin.spi.ConfigurationHook;
 
 public class SpiLoaderManagerImpl implements SpiLoaderManager {
 
+    private static final Logger log = LoggerFactory.getLogger(SpiLoaderManagerImpl.class);
+
     @Override
     public void load(RulerConfiguration configuration) {
         ServiceLoader<ConfigurationHook> loader = ServiceLoader.load(ConfigurationHook.class);
         for (ConfigurationHook hook : loader) {
-            hook.hook(configuration);
+            try {
+                hook.hook(configuration);
+            } catch (Exception e) {
+                e.printStackTrace();
+                log.error("load spi error", e);
+            }
         }
     }
 
